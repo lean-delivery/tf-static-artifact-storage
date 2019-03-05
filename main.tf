fd407cc6396051821ae8fb083f205b47377e57da
Arman Zh <arman_zholmyrzayev@epam.com>
2019-03-05 16:39:13 +0600

    fix

--- main.tf
+++ main.tf
@@ -88,13 +88,12 @@ resource "aws_cloudfront_distribution" "default" {
     s3_origin_config {
       origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
     }
-  }
-
-  custom_origin_config {
-    http_port              = "80"
-    https_port             = "443"
-    origin_protocol_policy = "https-only"
-    origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
   }
 
   logging_config {}
