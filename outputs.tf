6f3d9f179138033f1311887f823476b3fef2c031
Arman Zh <arman_zholmyrzayev@epam.com>
2019-03-06 14:18:37 +0600

    fmt

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "vpc_id" {
-  value = "${module.vpc.vpc_id}"
+  value       = "${module.vpc.vpc_id}"
   description = "VPC id"
 }
 
 output "whitelist" {
-  value = "${var.whitelist}"
+  value       = "${var.whitelist}"
   description = "List of whitelist CIDRs"
 }
